a557fcff9ac2ebeed3d4af9ca426442102067038
kim-se <kimse990507@gmail.com>
2023-06-21 16:34:53 +0900

    feat : first commit

---
+++ src/main/java/com/example/everymutsa/EveryMutsaApplication.java
@@ -0,0 +1,13 @@
+package com.example.everymutsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryMutsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EveryMutsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/everymutsa/EveryMutsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.everymutsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryMutsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
