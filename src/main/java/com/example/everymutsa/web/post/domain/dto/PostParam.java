fc104c5e4e10707dd4556ac104b6395727144876
twg0 <fruitsjh@gmail.com>
2023-06-27 00:46:48 +0900

    feat : dto 사용을 위한 메소드 추가 - Post.updateByParam - PostParam.createFromPost

--- src/main/java/com/example/everymutsa/web/post/domain/dto/PostParam.java
+++ src/main/java/com/example/everymutsa/web/post/domain/dto/PostParam.java
@@ -1,8 +1,13 @@
 package com.example.everymutsa.web.post.domain.dto;
 
+import com.example.everymutsa.web.post.domain.entity.Post;
+
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostParam {
 
 	String title;
@@ -19,5 +24,16 @@ public class PostParam {
 		this.language = language;
 		this.image = image;
 		this.heart = heart;
+	}
+
+	public static PostParam createFromPost(Post post) {
+		PostParam npp = new PostParam();
+		npp.title = post.getTitle();
+		npp.content = post.getContent();
+		npp.code = post.getCode();
+		npp.language = post.getLanguage();
+		npp.image = post.getImage();
+		npp.heart = post.getHeart();
+		return npp;
 	}
 }
