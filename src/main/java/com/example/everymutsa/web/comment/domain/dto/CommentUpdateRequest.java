93d258b0fe50a443e8fe9ce84cd240b366b49ebc
twg0 <fruitsjh@gmail.com>
2023-07-12 23:15:39 +0900

    fix: updateRequest Dto 수정

--- src/main/java/com/example/everymutsa/web/comment/domain/dto/CommentUpdateRequest.java
+++ src/main/java/com/example/everymutsa/web/comment/domain/dto/CommentUpdateRequest.java
@@ -1,12 +1,13 @@
 package com.example.everymutsa.web.comment.domain.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 public class CommentUpdateRequest {
 	private String content;
 
-	public CommentUpdateRequest(String Content) {
+	public CommentUpdateRequest(String content) {
 		this.content = content;
 	}
 }
