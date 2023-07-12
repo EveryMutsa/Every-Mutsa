a3c6c804036974d88a4019bcdf25970b6ec3405d
twg0 <fruitsjh@gmail.com>
2023-07-12 23:14:18 +0900

    feat : Comment Service 구현

--- src/main/java/com/example/everymutsa/web/comment/service/CommentService.java
+++ src/main/java/com/example/everymutsa/web/comment/service/CommentService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.everymutsa.common.exception.EntityNotFoundException;
 import com.example.everymutsa.common.exception.ErrorCode;
@@ -14,16 +15,20 @@ import com.example.everymutsa.web.comment.repository.CommentRepository;
 import com.example.everymutsa.web.member.domain.Member;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CommentService {
 
 	private final CommentRepository commentRepository;
 
-	public CommentResponse save(String content) {
+	@Transactional
+	public Long save(String content) {
 		Comment save = commentRepository.save(new Comment(content));
-		return new CommentResponse(save);
+		return save.getId();
 	}
 
 	public CommentResponse findById(Long id) {
@@ -33,13 +38,14 @@ public class CommentService {
 		return new CommentResponse(findComment);
 	}
 
+	@Transactional
 	public CommentResponse update(Long id, CommentUpdateRequest commentUpdateRequest) {
 		Comment comment = commentRepository.findByIdOrThrow(id);
 		comment.update(commentUpdateRequest.getContent());
-
 		return new CommentResponse(comment);
 	}
 
+	@Transactional
 	public void delete(Long id) {
 		commentRepository.deleteById(id);
 	}
