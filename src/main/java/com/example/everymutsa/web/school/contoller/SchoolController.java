55db3172b8fe5562972b304c57e4128dfd0ac771
sukeun95 <sukeun95@gmail.com>
2023-07-14 14:12:00 +0900

    feat.Class(School) Controller 수정

--- src/main/java/com/example/everymutsa/web/school/contoller/SchoolController.java
+++ src/main/java/com/example/everymutsa/web/school/contoller/SchoolController.java
@@ -1,11 +1,15 @@
 package com.example.everymutsa.web.school.contoller;
 
 import com.example.everymutsa.web.school.domain.dto.SchoolDto;
+import com.example.everymutsa.web.school.domain.entity.School;
 import com.example.everymutsa.web.school.service.SchoolService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -15,6 +19,24 @@ public class SchoolController {
 
     private final SchoolService schoolService;
 
+    @GetMapping("/{id}")
+    public SchoolDto readById(
+            @PathVariable Long id
+    ) {
+        School school = schoolService.findById(id);
+        return SchoolDto.fromEntity(school);
+    }
+
+    @GetMapping
+    public List<SchoolDto> readAllById(
+            Long id
+    ) {
+        List<School> schools = schoolService.findAllById(id);
+        return schools.stream()
+                .map(SchoolDto::fromEntity)
+                .collect(Collectors.toList());
+    }
+
     @PostMapping
     public SchoolDto create(
             @RequestBody SchoolDto dto
@@ -23,7 +45,7 @@ public class SchoolController {
         return SchoolDto.fromEntity(schoolService.findById(id));
     }
 
-    @PostMapping
+    @PostMapping("/{id}")
     public SchoolDto update(
             @PathVariable Long id,
             @RequestBody SchoolDto dto
@@ -31,7 +53,7 @@ public class SchoolController {
         return schoolService.update(id, dto);
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{id}")
     public ResponseEntity<String> delete(
             @PathVariable Long id
     ) {
