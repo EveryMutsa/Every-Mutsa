f01f4596af6d80560fd5319aeacebfbb1093a1cf
kim-se <kimse990507@gmail.com>
2023-07-18 20:58:12 +0900

    feat : Log 연관 관계 구현

--- src/main/java/com/example/everymutsa/web/log/domain/entity/Log.java
+++ src/main/java/com/example/everymutsa/web/log/domain/entity/Log.java
@@ -2,11 +2,13 @@ package com.example.everymutsa.web.log.domain.entity;
 
 import com.example.everymutsa.common.BaseEntity;
 
+import com.example.everymutsa.web.school.domain.entity.School;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 
 @Entity
-@Data
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "log")
 public class Log extends BaseEntity {
 	@Id
@@ -16,7 +18,7 @@ public class Log extends BaseEntity {
 	@Column(name = "content")
 	private String content;
 
-//	@ManyToOne(fetch = FetchType.LAZY)
-//	@JoinColumn(name = "school_id")
-//	private School school;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "school_id")
+	private School school;
 }
