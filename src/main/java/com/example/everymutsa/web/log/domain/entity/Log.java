df65e72de9c970ff9800dea712a70be96d75288f
김승은 <71924061+kse0312@users.noreply.github.com>
2023-07-19 14:26:16 +0900

    Bvmu 39 log dto생성 (#40)
    * feat : Log 연관 관계 구현
    
    * feat : LogEntity Dto 생성
    - Dto 생성
    - Dto, Entity Builder 구현

--- src/main/java/com/example/everymutsa/web/log/domain/entity/Log.java
+++ src/main/java/com/example/everymutsa/web/log/domain/entity/Log.java
@@ -2,6 +2,7 @@ package com.example.everymutsa.web.log.domain.entity;
 
 import com.example.everymutsa.common.BaseEntity;
 
+import com.example.everymutsa.web.log.domain.dto.LogDto;
 import com.example.everymutsa.web.school.domain.entity.School;
 import jakarta.persistence.*;
 import lombok.*;
@@ -21,4 +22,20 @@ public class Log extends BaseEntity {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "school_id")
 	private School school;
+
+
+	public static Log fromDto(LogDto request){
+		Log log = new Log();
+		log.id = request.getId();
+		log.content = request.getContent();
+		return log;
+	}
+
+	public void setContents(LogDto request){
+		this.content = request.getContent();
+	}
+
+	public void setSchool(School school){
+		this.school = school;
+	}
 }
