e5abf26a2d56b5e48bb09ad93ce1e21f04f83207
twg0 <fruitsjh@gmail.com>
2023-07-23 19:41:06 +0900

    fix : PostRepository 수정 - Pageable import 수정

--- src/main/java/com/example/everymutsa/web/post/repository/PostRepository.java
+++ src/main/java/com/example/everymutsa/web/post/repository/PostRepository.java
@@ -1,9 +1,8 @@
 package com.example.everymutsa.web.post.repository;
 
-import java.awt.print.Pageable;
-import java.util.List;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.everymutsa.common.exception.EntityNotFoundException;
