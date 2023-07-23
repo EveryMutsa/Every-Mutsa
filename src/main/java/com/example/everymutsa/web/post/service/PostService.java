2e180883264a7508777923de6ad4834095478abd
twg0 <fruitsjh@gmail.com>
2023-07-23 21:41:15 +0900

    fix : PostService 수정 - 명칭 통일 - 조회 기준에 따르는 메소드 구현 - 파일 <-> String 메소드 구현

--- src/main/java/com/example/everymutsa/web/post/service/PostService.java
+++ src/main/java/com/example/everymutsa/web/post/service/PostService.java
@@ -1,12 +1,27 @@
 package com.example.everymutsa.web.post.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.core.io.Resource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.example.everymutsa.web.post.domain.dto.PostParam;
+import com.example.everymutsa.api.file.dto.FileResponse;
+import com.example.everymutsa.api.file.service.FileService;
+import com.example.everymutsa.web.board.domain.entity.Board;
+import com.example.everymutsa.web.board.repository.BoardRepository;
+import com.example.everymutsa.web.member.domain.Member;
+import com.example.everymutsa.web.member.repository.MemberRepository;
+import com.example.everymutsa.web.post.domain.dto.PostRegister;
+import com.example.everymutsa.web.post.domain.dto.PostResponse;
+import com.example.everymutsa.web.post.domain.dto.PostUpdate;
 import com.example.everymutsa.web.post.domain.entity.Post;
 import com.example.everymutsa.web.post.repository.PostRepository;
 
@@ -18,20 +33,20 @@ import lombok.RequiredArgsConstructor;
 public class PostService {
 
 	private final PostRepository postRepository;
+	private final MemberRepository memberRepository;
+	private final BoardRepository boardRepository;
+	private final FileService fileService;
 
 	@Transactional
-	public Long save(PostParam postParam) {
-		Post post = Post.createPost(postParam);
-		postRepository.save(post);
-
-		return post.getId();
+	public PostResponse save(PostRegister postRegister) {
+		Post save = postRepository.save(postRegister.toDto(stringFromFiles(postRegister.getImages())));
+		return PostResponse.fromEntity(save);
 	}
 
 	@Transactional
-	public PostParam update(Long id, PostParam postParam) {
-		Post findPost = postRepository.findById(id).get();
-		findPost = Post.updateByParam(findPost, postParam);
-		return PostParam.createFromPost(findPost);
+	public void update(Long id, PostUpdate postUpdate) {
+		Post findPost = postRepository.findByIdOrThrow(id);
+		findPost.update(postUpdate, stringFromFiles(postUpdate.getImages()));
 	}
 
 	@Transactional
@@ -39,12 +54,49 @@ public class PostService {
 		postRepository.deleteById(id);
 	}
 
-	public List<Post> findPosts() {
-		return postRepository.findAll();
+	public PostResponse readOne(Long id) throws IOException {
+		Post post = postRepository.findByIdOrThrow(id);
+		PostResponse postResponse = PostResponse.fromEntity(post);
+		postResponse.setImages(filesFromString(post.getImageNames()));
+		return postResponse;
 	}
 
-	public Post findOne(Long id) {
-		return postRepository.findById(id).get();
+	public Page<PostResponse> readAllByMemberId(Long id, Pageable pageable) throws IOException {
+		Member member = memberRepository.findByIdOrThrow(id);
+		Page<Post> posts = postRepository.findAllByMember(member, pageable);
+		Page<PostResponse> postResponsePage = posts.map(PostResponse::fromEntity);
+		for (PostResponse postResponse : postResponsePage) {
+			postResponse.setImages(filesFromString(postResponse.getPreImages()));
+		}
+		return postResponsePage;
 	}
 
+	public Page<PostResponse> readAllByBoardId(Long id, Pageable pageable) throws IOException {
+		Board board = boardRepository.findByIdOrThrow(id);
+		Page<Post> posts = postRepository.findAllByBoard(board, pageable);
+		Page<PostResponse> postResponsePage = posts.map(PostResponse::fromEntity);
+		for (PostResponse postResponse : postResponsePage) {
+			postResponse.setImages(filesFromString(postResponse.getPreImages()));
+		}
+		return postResponsePage;
+	}
+
+
+	public String stringFromFiles(List<MultipartFile> list) {
+		StringBuilder images = new StringBuilder();
+		for (MultipartFile image : list) {
+			FileResponse store = fileService.store(image);
+			images.append(store.getFileName());
+		}
+		return images.toString();
+	}
+
+	public List<File> filesFromString(List<String> imageNames) throws IOException {
+		List<File> list = new ArrayList<>();
+		for (String imageName : imageNames) {
+			Resource resource = fileService.load(imageName);
+			list.add(resource.getFile());
+		}
+		return list;
+	}
 }
