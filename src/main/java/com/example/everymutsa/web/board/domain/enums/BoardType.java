8a731682054ac51c0da4a94582f97b7b0d24494c
eunhwa0308 <eunhwa0308@naver.com>
2023-07-24 01:14:00 +0900

    feat : Enum 문자열 추가

--- src/main/java/com/example/everymutsa/web/board/domain/enums/BoardType.java
+++ src/main/java/com/example/everymutsa/web/board/domain/enums/BoardType.java
@@ -1,7 +1,17 @@
 package com.example.everymutsa.web.board.domain.enums;
 
 public enum BoardType {
-	NOTICE,
-	QUESTION,
-	COMMUNITY
+	NOTICE("공지사항"),
+	QUESTION("질문사항"),
+	COMMUNITY("커뮤니티");
+
+	private final String type;
+
+	BoardType(String type) {
+		this.type = type;
+	}
+
+	public String getType() {
+		return this.type;
+	}
 }
