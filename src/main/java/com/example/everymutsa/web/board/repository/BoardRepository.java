f387e39075636dd1b4003cadba998e66d1a217b0
eunhwa0308 <eunhwa0308@naver.com>
2023-07-24 01:29:28 +0900

    fix : 파라미터 수정

--- src/main/java/com/example/everymutsa/web/board/repository/BoardRepository.java
+++ src/main/java/com/example/everymutsa/web/board/repository/BoardRepository.java
@@ -2,6 +2,7 @@ package com.example.everymutsa.web.board.repository;
 
 import com.example.everymutsa.common.exception.EntityNotFoundException;
 import com.example.everymutsa.common.exception.ErrorCode;
+import com.example.everymutsa.web.board.domain.enums.BoardType;
 import com.example.everymutsa.web.school.domain.entity.School;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,9 +13,9 @@ import com.example.everymutsa.web.board.domain.entity.Board;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
-    Page<Board> findAllBySchoolAndType(School school, String boardType, Pageable pageable);
+    Page<Board> findAllBySchoolAndType(School school, BoardType boardType, Pageable pageable);
 
-    Page<Board> findAllByType(String boardType);
+    Page<Board> findAllByType(BoardType boardType,Pageable pageable);
 
     default Board findByIdOrThrow(Long id){
         return findById(id)
