dc28657a7fd9b5b839c5cd8716543406cf8b5a8a
twg0 <76769151+twg0@users.noreply.github.com>
2023-07-23 20:34:42 +0900

    Bvmu 75 post repository fix (#55)
    * feat : Post Repository 수정
    - Member 기준 페이징 메소드 구현
    - Board 기준 페이징 메소드 구현
    
    * remove : PostParam 삭제

--- src/main/java/com/example/everymutsa/web/post/repository/PostRepository.java
+++ src/main/java/com/example/everymutsa/web/post/repository/PostRepository.java
@@ -1,8 +1,18 @@
 package com.example.everymutsa.web.post.repository;
 
+import java.awt.print.Pageable;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.everymutsa.web.board.domain.entity.BoardEntity;
+import com.example.everymutsa.web.member.domain.Member;
 import com.example.everymutsa.web.post.domain.entity.Post;
 
 public interface PostRepository extends JpaRepository<Post,Long> {
+
+	Page<Post> findAllByBoard(BoardEntity board, Pageable pageable);
+
+	Page<Post> findAllByMember(Member member, Pageable pageable);
 }
