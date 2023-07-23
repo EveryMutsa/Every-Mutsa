baf5a315519df672e5e1f10545178580036ee27d
twg0 <fruitsjh@gmail.com>
2023-07-23 22:13:57 +0900

    feat : PostController 재정의

--- src/main/java/com/example/everymutsa/web/post/controller/PostController.java
+++ src/main/java/com/example/everymutsa/web/post/controller/PostController.java
@@ -1,8 +1,11 @@
 package com.example.everymutsa.web.post.controller;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,10 +15,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.example.everymutsa.web.post.domain.dto.PostParam;
+import com.example.everymutsa.api.file.service.FileService;
+import com.example.everymutsa.web.post.domain.dto.PostRegister;
+import com.example.everymutsa.web.post.domain.dto.PostResponse;
+import com.example.everymutsa.web.post.domain.dto.PostUpdate;
 import com.example.everymutsa.web.post.domain.entity.Post;
 import com.example.everymutsa.web.post.service.PostService;
 
@@ -31,55 +38,69 @@ import lombok.extern.slf4j.Slf4j;
 public class PostController {
 
 	private final PostService postService;
-	@GetMapping
-	public Result readPostAll() {
 
-		List<Post> findPosts = postService.findPosts();
-		List<PostParam> collect = findPosts.stream()
-			.map(m -> PostParam.createFromPost(m))
-			.collect(Collectors.toList());
+	@GetMapping("{id}")
+	public ResponseEntity<PostResponse> readOneById(
+		@PathVariable("id") Long id
+	) {
+		try {
+			PostResponse postResponse = postService.readOne(id);
+			return ResponseEntity.ok(postResponse);
 
-		return new Result(collect);
-	}
-
-	@GetMapping("/{id}")
-	public PostParam readPostOne(@PathVariable Long id) {
-		try	{
-			Post findPost = postService.findOne(id);
-			return PostParam.createFromPost(findPost);
-		} catch (RuntimeException e) {
-			log.error("RuntimeException occurred : {} ", e.getMessage());
+		} catch (IOException e) {
+			log.error("readOne {}", e.getMessage());
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
 		}
-	}
-
-	@Data
-	@AllArgsConstructor
-	static class Result<T> {
-		private T data;
 	}
 
 	@PostMapping
-	public PostParam createPost(@RequestBody PostParam postParam) {
-		Long saveId = postService.save(postParam);
-		return PostParam.createFromPost(postService.findOne(saveId));
+	public ResponseEntity<PostResponse> createPost(@RequestBody PostRegister postRegister) {
+		PostResponse save = postService.save(postRegister);
+		return ResponseEntity.ok(save);
 	}
 
 	@PatchMapping("/{id}")
-	public PostParam updatePost(@PathVariable Long id, @RequestBody PostParam postParam) {
+	public ResponseEntity<Void> updatePost(@PathVariable Long id, @RequestBody PostUpdate postUpdate) {
 		log.info("id = {}", id);
-		PostParam updateParam = postService.update(id, postParam);
-		return updateParam;
+		postService.update(id, postUpdate);
+		return ResponseEntity.ok().build();
 	}
 
 	@DeleteMapping("/{id}")
-	public ResponseEntity<String> deletePost(@PathVariable Long id) {
+	public ResponseEntity<Void> deletePost(@PathVariable Long id) {
+		postService.remove(id);
+		return ResponseEntity.ok().build();
+	}
+
+	@GetMapping("members/{memberId}")
+	public Page<PostResponse> readAllByMemberId(
+		@PathVariable("memberId") Long memberId,
+		@RequestParam(name = "no", defaultValue = "0") Integer pageNo,
+		@RequestParam(name = "size", defaultValue = "20") Integer pageSize
+	) {
 		try {
-			postService.remove(id);
-		} catch(RuntimeException e) {
-			log.error("RuntimeException occurred : {}", e.getMessage());
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+			Page<PostResponse> postResponsePage =
+				postService.readAllByMemberId(memberId, PageRequest.of(pageNo, pageSize));
+			return postResponsePage;
+		} catch (IOException e) {
+			log.error("pageByMember error {}", e.getMessage());
+			throw new RuntimeException(e);
 		}
-		return ResponseEntity.ok("Deletion successful");
+	}
+
+	@GetMapping("boards/{boardId}")
+	public Page<PostResponse> readAllByboardId(
+		@PathVariable("boardId") Long boardId,
+		@RequestParam(name = "no", defaultValue = "0") Integer pageNo,
+		@RequestParam(name = "size", defaultValue = "20") Integer pageSize
+	) {
+		try {
+			Page<PostResponse> postResponsePage =
+				postService.readAllByBoardId(boardId, PageRequest.of(pageNo, pageSize));
+			return postResponsePage;
+		} catch (IOException e) {
+			log.error("pageByBoard error {}", e.getMessage());
+			throw new RuntimeException(e);
+		}
 	}
 }
