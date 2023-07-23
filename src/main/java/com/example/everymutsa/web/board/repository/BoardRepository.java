b4083caf9129475fcc74b617b869795dd262010c
eunhwa0308 <eunhwa0308@naver.com>
2023-07-23 20:04:05 +0900

    refactor: Board Repository 수정 및 추가

--- src/main/java/com/example/everymutsa/web/board/repository/BoardRepository.java
+++ src/main/java/com/example/everymutsa/web/board/repository/BoardRepository.java
@@ -1,10 +1,23 @@
 package com.example.everymutsa.web.board.repository;
 
+import com.example.everymutsa.common.exception.EntityNotFoundException;
+import com.example.everymutsa.common.exception.ErrorCode;
+import com.example.everymutsa.web.school.domain.entity.School;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.everymutsa.web.board.domain.entity.BoardEntity;
+import com.example.everymutsa.web.board.domain.entity.Board;
 
 @Repository
-public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    Page<Board> findAllBySchoolAndType(School school, String boardType, Pageable pageable);
+
+    Page<Board> findAllByType(String boardType);
+
+    default Board findByIdOrThrow(Long id){
+        return findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(ErrorCode.ENTITY_NOT_FOUND));
+    }
 }
