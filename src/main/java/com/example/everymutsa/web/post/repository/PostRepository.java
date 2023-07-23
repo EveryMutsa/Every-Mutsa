b013aa9cafe0ae52fb57ccf043dd4fb396b22869
twg0 <76769151+twg0@users.noreply.github.com>
2023-07-23 22:17:35 +0900

    Bvmu 76 post service fix (#59)
    * fix : Board 엔티티 수정으로 인한 Entity 및 Repository 수정
    
    * fix : PostService 수정
    - 명칭 통일
    - 조회 기준에 따르는 메소드 구현
    - 파일 <-> String 메소드 구현

--- src/main/java/com/example/everymutsa/web/post/repository/PostRepository.java
+++ src/main/java/com/example/everymutsa/web/post/repository/PostRepository.java
@@ -5,8 +5,8 @@ import com.example.everymutsa.common.exception.ErrorCode;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-import com.example.everymutsa.web.board.domain.entity.BoardEntity;
+
+import com.example.everymutsa.web.board.domain.entity.Board;
 import com.example.everymutsa.web.member.domain.Member;
 import com.example.everymutsa.web.post.domain.entity.Post;
 
@@ -15,7 +15,7 @@ public interface PostRepository extends JpaRepository<Post,Long> {
 		return findById(id)
 			.orElseThrow(() -> new EntityNotFoundException(ErrorCode.ENTITY_NOT_FOUND));
 	}
-	Page<Post> findAllByBoard(BoardEntity board, Pageable pageable);
+	Page<Post> findAllByBoard(Board board, Pageable pageable);
 
 	Page<Post> findAllByMember(Member member, Pageable pageable);
 }
