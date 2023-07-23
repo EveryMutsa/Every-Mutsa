cff93b45df7d277d78816e47bfb77d8508787061
twg0 <fruitsjh@gmail.com>
2023-07-23 17:56:19 +0900

    fix : PostRepository 수정 - findByOrThrow 메소드 추가

--- src/main/java/com/example/everymutsa/web/post/repository/PostRepository.java
+++ src/main/java/com/example/everymutsa/web/post/repository/PostRepository.java
@@ -6,11 +6,19 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.everymutsa.common.exception.EntityNotFoundException;
+import com.example.everymutsa.common.exception.ErrorCode;
 import com.example.everymutsa.web.board.domain.entity.BoardEntity;
 import com.example.everymutsa.web.member.domain.Member;
 import com.example.everymutsa.web.post.domain.entity.Post;
+import com.example.everymutsa.web.school.domain.entity.School;
 
 public interface PostRepository extends JpaRepository<Post,Long> {
+
+	default Post findByIdOrThrow(Long id) {
+		return findById(id)
+			.orElseThrow(() -> new EntityNotFoundException(ErrorCode.ENTITY_NOT_FOUND));
+	}
 
 	Page<Post> findAllByBoard(BoardEntity board, Pageable pageable);
 
